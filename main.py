d2904596d3918d939c73c01d5d15fc2f5f1948a8
root <root@ntp.home>
2023-12-17 23:40:09 -0300

    chore: remove battery charger support from main.py.

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from time import sleep
 import datetime
 import gpsd
 from vfd import BufferedVFD, Brightness, Direction
-from batt_gauge import MAX17040
 from enum import Enum
 from time import monotonic
 import signal
-
-State = Enum('State', ['BATT', 'GPSPOS'])
 
 class Main:
     UPDATE_RATE = 0.05
@@ -25,16 +22,12 @@ class Main:
         DB6 = 24
         DB7 = 25
 
-        self.batt = MAX17040(1)
-
         self.disp = BufferedVFD(RS, E, DB7, DB6, DB5, DB4)
         self.disp.initialize()
         self.disp.control(display=1, cursor=0, blink=0)
         self.disp.brightness(Brightness.LOW)
 
         gpsd.connect()
-
-        self._state = State.BATT
 
     def __enter__(self):
         return self
@@ -59,25 +52,16 @@ class Main:
 
         return f"{lat}{lat_post} {lon}{lon_post} {alt}m"
 
-    def render_batt(self):
-        battlevel = round(self.batt.charge, 1)
-        battvolt = round(self.batt.voltage, 1)
-        return f"BATT {battlevel}% {battvolt}V"
-
     def render_datetime(self):
         return datetime.datetime.now().strftime('%c')
 
     def render(self):
-        #self.disp.clear()
         self.disp.clear()
         self.disp.setpos(0, 0)
         self.disp.write(self.render_datetime())
 
         self.disp.setpos(1,0)
-        if self._state is State.BATT:
-            self.disp.write(self.render_batt())
-        elif self._state is State.GPSPOS:
-            self.disp.write(self.render_pos())
+        self.disp.write(self.render_pos())
 
         self.disp.update()
 
@@ -85,13 +69,6 @@ class Main:
         last_state_change = monotonic()
         last_shift = monotonic()
         while not self.stopped:
-            if monotonic() - last_state_change > self.PAGE_DELAY:
-                last_state_change = monotonic()
-                if self._state is State.BATT:
-                    self._state = State.GPSPOS
-                elif self._state is State.GPSPOS:
-                    self._state = State.BATT
-
             if monotonic() - last_shift > self.SCROLL_INTERVAL:
                 last_shift = monotonic()
                 self.disp.shift_display(Direction.LEFT)
